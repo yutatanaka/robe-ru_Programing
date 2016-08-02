04c844024e3f1a4f5b8f466e2c19351f20b61197
yutatanaka <koryotennis99@gmail.com>
2016-08-02 19:22:09 +0900

    4つ目のパターン

---
+++ C++_Programing/2-12_複雑に回る_その４.cpp
@@ -0,0 +1,24 @@
+
+/*
+	4‚Â–Ú‚Ìƒpƒ^[ƒ“
+	
+	i‚ğíœ‚µ‚Ä‚İ‚é
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 0;
+
+	for (; a < 10;)
+	{
+		cout << a << ' ';
+		a += 2;
+	}
+	cout << endl;
+
+	getchar();
+	return 0;
+}

8af45041b47f62dfb2cb0b666595e234e5c3d4ad
yutatanaka <koryotennis99@gmail.com>
2016-08-02 19:13:40 +0900

    2つ目のパターン

---
+++ C++_Programing/2-12_複雑に回る_その２.cpp
@@ -0,0 +1,21 @@
+
+/*
+	２つめのパターン
+
+	i＝10000から始めて、iは１ずつ増やす
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	for (i = 10000; i < 10; i++)
+	{
+		cout << i << endl;
+	}
+	
+	getchar();
+	return 0;
+}

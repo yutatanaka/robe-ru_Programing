5729f8ec011f5bbf7b0599fe66898cc00feac2ec
yutatanaka <koryotennis99@gmail.com>
2016-08-05 11:09:39 +0900

    文字列走査関数その２

---
+++ C++_Programing/3-05_文字列をいじろう_その２.cpp
@@ -0,0 +1,30 @@
+
+/*
+	•¶š—ñ‘€ìŠÖ”‚»‚Ì‚Q
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int f(int x, int y)
+{
+	return 2 * x + y;
+}
+
+void Show(int x, int y)
+{
+	char str[50];
+
+	sprintf(str, "f(%d, %d) = %d", x, y, f(x, y));
+	cout << str << endl;
+}
+
+int main()
+{
+	Show(1, 2);
+	Show(182, 144);
+	getchar();
+	return 0;
+}
